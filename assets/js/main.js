514cec3651f7c50261ffb957263b4d049c8a4865
hutamatr <hutamtr@gmail.com>
2021-12-15 01:53:57 +0700

    Add Header & Menu for Mobile

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,48 @@
 /*=============== SHOW MENU ===============*/
 
+const navMenu = document.getElementById('nav-menu'),
+  navToggle = document.getElementById('nav-toggle'),
+  navClose = document.getElementById('nav-close');
+
+//   Menu Show
+
+/* Validation if COntent Exist*/
+
+if (navToggle) {
+  navToggle.addEventListener('click', () => {
+    navMenu.classList.add('show-menu');
+  });
+}
+
+// Menu Hidden
+
+if (navClose) {
+  navClose.addEventListener('click', () => {
+    navMenu.classList.remove('show-menu');
+  });
+}
 
 /*=============== REMOVE MENU MOBILE ===============*/
 
+const navLink = document.querySelectorAll('.nav__link');
 
+let linkAction = () => {
+  const navMenu = document.getElementById('nav-menu');
+
+  // When CLick on each nav__link, remove show_menu class
+
+  navMenu.classList.remove('show-menu');
+};
+
+navLink.forEach((item) => item.addEventListener('click', linkAction));
 /*=============== CHANGE BACKGROUND HEADER ===============*/
-
 
 /*=============== QUESTIONS ACCORDION ===============*/
 
-
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
+/*=============== SHOW SCROLL UP ===============*/
 
-/*=============== SHOW SCROLL UP ===============*/ 
-
-
-/*=============== DARK LIGHT THEME ===============*/ 
-
+/*=============== DARK LIGHT THEME ===============*/
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
