2cb7ae38605c192886ecbd468b884138a97866e0
hutamatr <hutamatr@gmail.com>
2021-12-20 14:39:06 +0700

    Add about section for Mobile & button

--- assets/js/main.js
+++ assets/js/main.js
@@ -35,8 +35,19 @@ let linkAction = () => {
 };
 
 navLink.forEach((item) => item.addEventListener('click', linkAction));
-/*=============== CHANGE BACKGROUND HEADER ===============*/
+/*=============== ADD SCROLL HEADER SHADOW ===============*/
 
+function scrollHeader() {
+  const header = document.getElementById('header');
+
+  if (this.scrollY >= 80) {
+    header.classList.add('scroll-header');
+  } else {
+    header.classList.remove('scroll-header');
+  }
+}
+
+window.addEventListener('scroll', scrollHeader);
 /*=============== QUESTIONS ACCORDION ===============*/
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
